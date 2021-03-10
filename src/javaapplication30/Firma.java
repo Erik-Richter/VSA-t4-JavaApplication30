b45e31c5bed940e4861513da9d1f47a55f62e0e1
erik-ctrl <richtererik20@gmail.com>
2021-03-10 19:23:52 +0100

    initial commit

---
+++ src/javaapplication30/Firma.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication30;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author vsa
+ */
+@Entity
+@Table(name = "FIRMA")
+@NamedQueries({
+    @NamedQuery(name = "Firma.findAll", query = "SELECT f FROM Firma f")})
+public class Firma implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "NAZOV")
+    private String nazov;
+    @OneToMany(mappedBy = "vydavatelId")
+    private Collection<Kniha> knihaCollection;
+
+    public Firma() {
+    }
+
+    public Firma(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNazov() {
+        return nazov;
+    }
+
+    public void setNazov(String nazov) {
+        this.nazov = nazov;
+    }
+
+    public Collection<Kniha> getKnihaCollection() {
+        return knihaCollection;
+    }
+
+    public void setKnihaCollection(Collection<Kniha> knihaCollection) {
+        this.knihaCollection = knihaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Firma)) {
+            return false;
+        }
+        Firma other = (Firma) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaapplication30.Firma[ id=" + id + " ]";
+    }
+    
+}
